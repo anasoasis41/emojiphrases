0fa9c39505befe4becf894dab302c3071cce57ac
Anas RIAHI <anas.riahi@smile-maroc.com>
2020-02-11 16:40:31 +0100

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,25 @@
+package com.riahi
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+
+        get("/hello") {
+            call.respondText("Hello, Ktor")
+        }
+    }
+}
+
